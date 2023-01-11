69c562feb8b59d77fafb8b2c8b40d71eee5c8a0a
kosov <kosov941@gmail.com>
2023-01-11 11:08:58 +0700

    JobService

--- app/src/main/java/com/example/servicestest/MainActivity.kt
+++ app/src/main/java/com/example/servicestest/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.servicestest
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.app.job.JobWorkItem
+import android.content.ComponentName
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -11,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    var page = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -21,12 +28,30 @@ class MainActivity : AppCompatActivity() {
         binding.foregroundService.setOnClickListener {
             ContextCompat.startForegroundService(
                 this,
-                MyForegroundService.newIntent(this))
+                MyForegroundService.newIntent(this)
+            )
         }
         binding.intentService.setOnClickListener {
             ContextCompat.startForegroundService(
                 this,
-                MyIntentService.newIntent(this))
+                MyIntentService.newIntent(this)
+            )
+        }
+        binding.jobScheduler.setOnClickListener {
+            val componentName = ComponentName(this, MyJobService::class.java)
+
+            val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .build()
+
+            val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val intent = MyJobService.newIntent(page++)
+                jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
+            } else {
+                startService(MyIntentService2.newIntent(this, page++))
+            }
         }
 
     }
---
+++ app/src/main/java/com/example/servicestest/MyIntentService2.kt
@@ -0,0 +1,47 @@
+package com.example.servicestest
+
+import android.app.IntentService
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class MyIntentService2 : IntentService(NAME) {
+
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onHandleIntent(intent: Intent?) {
+        log("onHandleIntent")
+        val page = intent?.getIntExtra(PAGE,0) ?: 0
+        for (i in 0 until 100) {
+            Thread.sleep(1000)
+            log("Timer $i $page")
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+    }
+
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyForegroundService: $message")
+    }
+
+
+    companion object {
+        const val NAME = "MyIntentService"
+        const val PAGE = "page"
+
+        fun newIntent(context: Context, page: Int): Intent {
+            return Intent(context, MyIntentService2::class.java).apply {
+                putExtra(PAGE, page)
+            }
+        }
+    }
+}
